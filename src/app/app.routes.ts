0f0ebdced89cc9e9b03d57961fd8b1de19377631
Bohdan Zhykhor <bohdanzhyhor@gmail.com>
2024-05-09 12:19:20 -0400

    custom initial commit (restored source code from task 3.2)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+];
