a3b09975953ac57e74a3d9858dfa18bcfc9d2f4a
Bohdan Zhykhor <bohdanzhyhor@gmail.com>
2024-05-20 12:22:35 -0400

    task 3.3 version 2: small code refactoring

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../product/product.component';
 import { ShopDataService } from '../../services/shop-data.service';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Product } from '../../models/product';
 
 @Component({
@@ -14,15 +14,16 @@ import { Product } from '../../models/product';
 })
 export class HomeComponent {
   allProducts$: Observable<Product[]>;
-  specialProducts: Product[] = [];
-  products: Product[] = [];
+  specialProducts$: Observable<Product[]>;
+  products$: Observable<Product[]>;
 
   constructor(private shopDataService: ShopDataService) {
     this.allProducts$ = this.shopDataService.getAllProducts();
-    this.allProducts$.subscribe((products) => {
-      // Update specialProducts and products whenever allProducts$ changes
-      this.specialProducts = products.filter((product) => product.main);
-      this.products = products.filter((product) => !product.main);
-    });
+    this.specialProducts$ = this.allProducts$.pipe(
+      map((products) => products.filter((product) => product.main))
+    );
+    this.products$ = this.allProducts$.pipe(
+      map((products) => products.filter((product) => !product.main))
+    );
   }
 }
