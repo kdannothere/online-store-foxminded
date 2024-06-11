9a4255862ab632e0fb528aaece06e8619b822e41
Bohdan Zhykhor <bohdanzhyhor@gmail.com>
2024-06-11 12:50:29 -0400

    Contact page now uses a Reactive Forms, not Template-Driven Forms

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -8,19 +14,46 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
-  name = '';
-  surname = '';
-  email = '';
-  message = '';
+  contactForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(1),
+      Validators.maxLength(33),
+    ]),
+    surname: new FormControl('', [
+      Validators.required,
+      Validators.minLength(1),
+      Validators.maxLength(33),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(1000),
+    ]),
+  });
 
-	get valid(): boolean {
-		return this.name.length > 0 && this.surname.length > 0 && this.email.length > 0 && this.message.length > 0
-	}
+  get valid(): boolean {
+    const controls = this.contactForm.controls;
+    return (
+      controls.name.valid &&
+      controls.surname.valid &&
+      controls.email.valid &&
+      controls.message.valid
+    );
+  }
 
   sendMessage() {}
 }
